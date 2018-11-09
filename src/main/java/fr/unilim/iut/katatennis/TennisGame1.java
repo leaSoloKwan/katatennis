82d89ef628d49cb5433d3b6d7ad760ed09e593ed
SOLO KWAN <lea.solo@etu.unilim.fr>
2018-11-09 17:06:52 +0100

    Refactor méthode getScore()

--- src/main/java/fr/unilim/iut/katatennis/TennisGame1.java
+++ src/main/java/fr/unilim/iut/katatennis/TennisGame1.java
@@ -21,66 +21,85 @@ public class TennisGame1 implements TennisGame {
 
 	public String getScore() {
 		String score = "";
+		
+		
+		if (mScore1 == mScore2) {
+			score = scoreEgaux();
+		} else if (mScore1 >= 4 || mScore2 >= 4) {
+			score = scoreSupQuatre();
+		} else {
+			score = scoreInfQuatre(score);
+		}
+		return score;
+	}
+
+	public String scoreInfQuatre(String score) {
 		int tempScore = 0;
 		StringBuilder bld=new StringBuilder();
-		if (mScore1 == mScore2) {
-			switch (mScore1) {
+		for (int i = 1; i < 3; i++) {
+			if (i == 1)
+				tempScore = mScore1;
+			else {
+				bld.append("-");
+				
+				tempScore = mScore2;
+			}
+			switch (tempScore) {
 			case 0:
-				score = "Love-All";
+				bld.append("Love");
+				
 				break;
 			case 1:
-				score = "Fifteen-All";
+				bld.append("Fifteen");
+				
 				break;
 			case 2:
-				score = "Thirty-All";
+				bld.append("Thirty");
+				
 				break;
-			default:
-				score = "Deuce";
+			case 3:
+				bld.append("Forty");
+				
 				break;
+			default :
+				
+				break;
+			}
+			score=bld.toString();
+		}
+		return score;
+	}
 
-			}
-		} else if (mScore1 >= 4 || mScore2 >= 4) {
-			int minusResult = mScore1 - mScore2;
-			if (minusResult == 1)
-				score = "Advantage player1";
-			else if (minusResult == -1)
-				score = "Advantage player2";
-			else if (minusResult >= 2)
-				score = "Win for player1";
-			else
-				score = "Win for player2";
-		} else {
-			for (int i = 1; i < 3; i++) {
-				if (i == 1)
-					tempScore = mScore1;
-				else {
-					bld.append("-");
-					
-					tempScore = mScore2;
-				}
-				switch (tempScore) {
-				case 0:
-					bld.append("Love");
-					
-					break;
-				case 1:
-					bld.append("Fifteen");
-					
-					break;
-				case 2:
-					bld.append("Thirty");
-					
-					break;
-				case 3:
-					bld.append("Forty");
-					
-					break;
-				default :
-					
-					break;
-				}
-				score=bld.toString();
-			}
+	public String scoreSupQuatre() {
+		String score;
+		int minusResult = mScore1 - mScore2;
+		if (minusResult == 1)
+			score = "Advantage player1";
+		else if (minusResult == -1)
+			score = "Advantage player2";
+		else if (minusResult >= 2)
+			score = "Win for player1";
+		else
+			score = "Win for player2";
+		return score;
+	}
+
+	public String scoreEgaux() {
+		String score;
+		switch (mScore1) {
+		case 0:
+			score = "Love-All";
+			break;
+		case 1:
+			score = "Fifteen-All";
+			break;
+		case 2:
+			score = "Thirty-All";
+			break;
+		default:
+			score = "Deuce";
+			break;
+
 		}
 		return score;
 	}
