a37e08b146010a952fd451abc66f0bc9f5d943e7
SOLO KWAN <lea.solo@etu.unilim.fr>
2018-11-09 16:38:59 +0100

    Refactor mScore1 et mScore2

--- src/main/java/fr/unilim/iut/katatennis/TennisGame1.java
+++ src/main/java/fr/unilim/iut/katatennis/TennisGame1.java
@@ -2,8 +2,8 @@ package fr.unilim.iut.katatennis;
 
 public class TennisGame1 implements TennisGame {
 
-	private int m_score1 = 0;
-	private int m_score2 = 0;
+	private int mScore1 = 0;
+	private int mScore2 = 0;
 	private String player1Name;
 	private String player2Name;
 
@@ -14,16 +14,16 @@ public class TennisGame1 implements TennisGame {
 
 	public void wonPoint(String playerName) {
 		if (playerName == "player1")
-			m_score1 += 1;
+			mScore1 += 1;
 		else
-			m_score2 += 1;
+			mScore2 += 1;
 	}
 
 	public String getScore() {
 		String score = "";
 		int tempScore = 0;
-		if (m_score1 == m_score2) {
-			switch (m_score1) {
+		if (mScore1 == mScore2) {
+			switch (mScore1) {
 			case 0:
 				score = "Love-All";
 				break;
@@ -38,8 +38,8 @@ public class TennisGame1 implements TennisGame {
 				break;
 
 			}
-		} else if (m_score1 >= 4 || m_score2 >= 4) {
-			int minusResult = m_score1 - m_score2;
+		} else if (mScore1 >= 4 || mScore2 >= 4) {
+			int minusResult = mScore1 - mScore2;
 			if (minusResult == 1)
 				score = "Advantage player1";
 			else if (minusResult == -1)
@@ -51,10 +51,10 @@ public class TennisGame1 implements TennisGame {
 		} else {
 			for (int i = 1; i < 3; i++) {
 				if (i == 1)
-					tempScore = m_score1;
+					tempScore = mScore1;
 				else {
 					score += "-";
-					tempScore = m_score2;
+					tempScore = mScore2;
 				}
 				switch (tempScore) {
 				case 0:
